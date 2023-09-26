4ddeba7e071b2ef76617e420806d2729b43db489
asam8733260 <subairasam8733260@gmail.com>
2023-09-26 17:18:51 +0530

    first sprint

---
+++ firstsections/About.js
@@ -0,0 +1,44 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { TypingText } from '../firstcomponents';
+
+import styles from '../app';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10 `}>
+    
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| About Metaversus" textStyles="text-center" />
+
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-black"
+      >
+        <span className="font-extrabold text-green-900">Metaverse</span> is a new
+        thing in the future, where you can enjoy the virtual world by feeling
+        like it's really real, you can feel what you feel in this metaverse
+        world, because this is really the{' '}
+        <span className="font-extrabold text-green-900">
+          madness of the metaverse
+        </span>{' '}
+        of today, using only{' '}
+        <span className="font-extrabold text-green-900">VR</span> devices you can
+        easily explore the metaverse world you want, turn your dreams into
+        reality. Let's{' '}
+        <span className="font-extrabold text-green-900">explore</span> the madness
+        of the metaverse by scrolling down
+      </motion.p>
+
+    </motion.div>
+  </section>
+);
+
+export default About;
